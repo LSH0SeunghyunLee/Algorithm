6e57a21b2d4a8c36d6fcdf72e0b491b8e2e1f147
LSH0SeunghyunLee <104187750+LSH0SeunghyunLee@users.noreply.github.com>
2022-05-26 20:36:17 +0900

    [Silver IV] Title: 큐, Time: 376 ms, Memory: 20760 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Queue<String> queue = new LinkedList<>();
+
+        int num = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < num; i++) {
+            String[] input = br.readLine().split(" ");
+            switch (input[0]) {
+                case "push":
+                    queue.add(String.valueOf(input[1]));
+                    break;
+                case "pop":
+                    if (queue.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(queue.poll());
+                    }
+                    break;
+                case "size":
+                    System.out.println(queue.size());
+                    break;
+                case "empty":
+                    if (queue.isEmpty()) {
+                        System.out.println(1);
+                    } else {
+                        System.out.println(0);
+                    }
+                    break;
+                case "front":
+                    if (queue.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(queue.peek());
+                    }
+                    break;
+                case "back":
+                    if (queue.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(((LinkedList<String>) queue).getLast());
+                    }
+                    break;
+            }
+        }
+        br.close();
+
+    }
+}
