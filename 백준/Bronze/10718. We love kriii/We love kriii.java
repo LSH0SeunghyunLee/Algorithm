c70b8006c817d17ba7c4c89864f9656c25ac29bc
leverest96 <104187750+leverest96@users.noreply.github.com>
2022-10-23 16:30:14 +0900

    [Bronze V] Title: We love kriii, Time: 116 ms, Memory: 13860 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String A = "강한친구 대한육군";
+
+        System.out.println(A);
+        System.out.println(A);
+    }
+}
