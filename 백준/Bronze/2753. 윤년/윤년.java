6abcc71d3a5f029f3c71924918df03adff921cf7
leverest96 <104187750+leverest96@users.noreply.github.com>
2022-10-26 02:24:54 +0900

    [Bronze V] Title: 윤년, Time: 128 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        int A = Integer.parseInt(in.readLine());
+
+        if (A % 4 == 0) {
+            if (A % 400 == 0) {
+                System.out.println("1");
+            } else if (A % 100 == 0) {
+                System.out.println("0");
+            } else {
+                System.out.println("1");
+            }
+        } else {
+            System.out.println("0");
+        }
+        in.close();
+    }
+}
