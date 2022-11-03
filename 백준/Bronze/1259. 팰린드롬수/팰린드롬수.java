ba6a0d36dc5b4552cdf8cecd5079af5600aeac11
leverest96 <104187750+leverest96@users.noreply.github.com>
2022-11-04 01:55:11 +0900

    [Bronze I] Title: 팰린드롬수, Time: 120 ms, Memory: 14040 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -6,28 +6,15 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         while (true) {
-            String answer = "no";
+            String answer = "yes";
 
             String N = in.readLine();
 
-            char[] list = N.toCharArray();
-
             if (Integer.parseInt(N) == 0) {
                 break;
-            } else if (N.length() % 2 == 0) {
-                for (int i = 0; i < N.length()/2; i++) {
-                    if (list[i] == list[N.length() - (i + 1)]) {
-                        answer = "yes";
-                    } else {
-                        answer = "no";
-                        break;
-                    }
-                }
             } else {
-                for (int i = 0; i < N.length()/2 + 1; i++) {
-                    if (list[i] == list[N.length() - (i + 1)]) {
-                        answer = "yes";
-                    } else {
+                for (int i = 0; i < N.length()/2; i++) {
+                    if (N.charAt(i) != N.charAt(N.length() - (i + 1))) {
                         answer = "no";
                         break;
                     }
