0306ef8f90aacf85fd845d5932baca57ffe20694
leverest96 <104187750+leverest96@users.noreply.github.com>
2023-02-21 15:08:04 +0900

    [Bronze II] Title: OX퀴즈, Time: 84 ms, Memory: 11680 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -1,28 +1,39 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int A = Integer.parseInt(in.readLine());
-
-        for (int i = 0; i < A; i++) {
-            String B = in.readLine();
-
-            int C = 0;
-            int D = 0;
-
-            for (int j = 0; j < B.length(); j++) {
-                if (B.charAt(j) == 'O') {
-                    C++;
-                    D += C;
-                } else {
-                    C = 0;
-                }
-            }
-            System.out.println(D);
-        }
+        int testcase = Integer.parseInt(in.readLine());
+        
+        for (int tc = 0; tc < testcase; tc++) {
+        	String line = in.readLine();
+            
+            char[] arr = line.toCharArray();
+            
+            int sum = 0;
+            int cnt = 0;
+            
+            for (int i = 0; i < arr.length; i++) {
+				if (arr[i] == 'O') {
+					cnt++;
+					sum += cnt;
+				} else {
+					cnt = 0;
+				}
+			}
+            
+            out.write(String.valueOf(sum));
+            out.newLine();
+		}
 
         in.close();
+        out.flush();
+        out.close();
     }
 }
