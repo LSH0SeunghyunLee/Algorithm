94250bbbc65933f19c6ab4c0423508c2c1f09b58
leverest96 <104187750+leverest96@users.noreply.github.com>
2023-05-10 22:18:33 +0900

    [Silver IV] Title: 수 찾기, Time: 2056 ms, Memory: 79420 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -3,49 +3,43 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
-
-    public static int[] arr;
+    static int N;
+    static int[] arr;
 
     public static void main(String[] args) throws IOException {
-        int N = Integer.parseInt(in.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer nTokenizer = new StringTokenizer(in.readLine());
+        N = Integer.parseInt(br.readLine());
 
         arr = new int[N];
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(nTokenizer.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(arr);
 
-        int K = Integer.parseInt(in.readLine());
+        int M = Integer.parseInt(br.readLine());
 
-        StringTokenizer kTokenizer = new StringTokenizer(in.readLine());
+        st = new StringTokenizer(br.readLine());
 
-        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            int result = binarySearch(Integer.parseInt(st.nextToken()));
 
-        for(int i = 0; i < K; i++) {
-            if(binarySearch(Integer.parseInt(kTokenizer.nextToken())) >= 0) {
-                stringBuilder.append(1).append('\n');
-            }
-            else {
-                stringBuilder.append(0).append('\n');
-            }
+            if (result >= 0) System.out.println(1);
+            else System.out.println(0);
         }
 
-        out.write(String.valueOf(stringBuilder));
-
-        in.close();
-        out.flush();
-        out.close();
+        bw.flush();
+        bw.close();
     }
 
     public static int binarySearch(int key) {
         int lo = 0;
-        int hi = arr.length - 1;
+        int hi = N - 1;
 
         while (lo <= hi) {
             int mid = (lo + hi) / 2;
