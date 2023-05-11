68eab1881a871305f54efed8abcfbb0a5ef5a5b3
leverest96 <104187750+leverest96@users.noreply.github.com>
2023-05-12 01:29:44 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 716 ms, Memory: 126596 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,21 +1,22 @@
 import java.io.*;
-import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        boolean[] arr = new boolean[2000001];
+
         int N = Integer.parseInt(br.readLine());
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-
-        for (int i = 0; i < N; i++) {
-            pq.add(Integer.parseInt(br.readLine()));
+        for(int i = 0; i < N; i++) {
+            arr[Integer.parseInt(br.readLine()) + 1000000] = true;
         }
 
-        for (int i = 0; i < N; i++) {
-            bw.write(pq.poll() + "\n");
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i]) {
+                bw.write((i-1000000) + "\n");
+            }
         }
         bw.flush();
         bw.close();
